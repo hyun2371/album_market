b81a977a5b12fd0167f19b393956739597945afb
hs12 <loveanna1228@gmail.com>
2023-11-06 22:03:48 +0900

    [Setting] 프로젝트 초기 설정

---
+++ album_market/src/main/java/com/prgrms/album_market/AlbumMarketApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.album_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbumMarketApplication.class, args);
+    }
+
+}

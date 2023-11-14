0d08dbdc0ce5c858b8d620fa8f9c9256112e97e9
hs12 <loveanna1228@gmail.com>
2023-11-14 23:53:09 +0900

    [Refactor] 불필요한 코드 삭제

--- album_market/src/main/java/com/prgrms/album_market/common/exception/ErrorCode.java
+++ album_market/src/main/java/com/prgrms/album_market/common/exception/ErrorCode.java
@@ -8,53 +8,14 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    // Member
-    EMPTY_INPUT_EMAIL(HttpStatus.BAD_REQUEST, "이메일을 입력해주세요."),
-
-    EMPTY_INPUT_NAME(HttpStatus.BAD_REQUEST, "이름을 입력해주세요."),
-
-    EMPTY_INPUT_PWD(HttpStatus.BAD_REQUEST, "비밀번호를 입력해주세요."),
-
-    EMPTY_INPUT_PHONE_NUMBER(HttpStatus.BAD_REQUEST, "휴대폰 번호를 입력해주세요."),
-
-    EMPTY_INPUT_CITY(HttpStatus.BAD_REQUEST, "도시를 입력해주세요."),
-
-    EMPTY_INPUT_STREET(HttpStatus.BAD_REQUEST, "도로를 입력해주세요."),
-
-    EMPTY_INPUT_ZIPCODE(HttpStatus.BAD_REQUEST, "우편번호를 입력해주세요."),
-
-
-    INVALID_FORMAT_EMAIL(HttpStatus.BAD_REQUEST, "이메일 형식이 올바르지 않습니다."),
-
-    INVALID_FORMAT_PWD(HttpStatus.BAD_REQUEST, "비밀번호 형식이 올바르지 않습니다."),
-
-    INVALID_FORMAT_PN(HttpStatus.BAD_REQUEST, "휴대폰 번호 형식이 올바르지 않습니다."),
-
-    INVALID_FORMAT_ZIP_CODE(HttpStatus.BAD_REQUEST, "우편번호 형식이 올바르지 않습니다."),
-
-
     NOT_EXIST_MEMBER_ID(HttpStatus.NOT_FOUND, "해당 아이디의 회원이 존재하지 않습니다."),
+    NOT_EXIST_MEMBER_EMAIL(HttpStatus.NOT_FOUND, "해당 이메일의 회원이 존재하지 않습니다."),
     ALREADY_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "동일한 이메일의 회원이 이미 존재합니다."),
 
     WRONG_MEMBER_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 올바르지 않습니다"),
 
     NOT_ENOUGH_MONEY(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
     INVALID_FORMAT_MONEY(HttpStatus.BAD_REQUEST, "1000원부터 충전이 가능합니다."),
-
-
-    // Album
-    EMPTY_INPUT_TITLE(HttpStatus.BAD_REQUEST, "제목을 입력해주세요."),
-
-    EMPTY_INPUT_ARTIST(HttpStatus.BAD_REQUEST, "아티스트를 입력해주세요."),
-
-    EMPTY_INPUT_CATEGORY(HttpStatus.BAD_REQUEST, "카테고리를 입력해주세요."),
-
-    EMPTY_INPUT_IMG_URL(HttpStatus.BAD_REQUEST, "이미지 주소를 입력해주세요."),
-
-    EMPTY_INPUT_RELEASE_DATE(HttpStatus.BAD_REQUEST, "발매일을 입력해주세요."),
-
-    EMPTY_INPUT_PRICE(HttpStatus.BAD_REQUEST, "가격을 입력해주세요."),
-
 
     INVALID_FORMAT_CATEGORY(HttpStatus.BAD_REQUEST, "카테고리 형식이 올바르지 않습니다."),
 
@@ -80,10 +41,6 @@ public enum ErrorCode {
     CANCELED_CAN_NOT_DELIVER(HttpStatus.BAD_REQUEST, "이미 취소된 앨범은 배달할 수 없습니다."),
 
     ALREADY_CANCELED_ALBUM(HttpStatus.BAD_REQUEST, "이미 취소된 주문입니다."),
-
-    ALREADY_DELIVERED_ALBUM(HttpStatus.BAD_REQUEST, "이미 배달 완료된 앨범입니다."),
-
-    INVALID_FORMAT_QUANTITY(HttpStatus.BAD_REQUEST, "최소 주문 수량은 1개입니다."),
 
     OUT_OF_STOCK(HttpStatus.BAD_REQUEST, "재고가 부족합니다."),
 

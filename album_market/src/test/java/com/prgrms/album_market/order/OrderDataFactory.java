45e14193a8edf27da353928ba0d1d63a46ca667c
hs12 <loveanna1228@gmail.com>
2023-11-21 17:09:49 +0900

    [Feat] 주문 테스트 로직 추가

--- album_market/src/test/java/com/prgrms/album_market/order/OrderDataFactory.java
+++ album_market/src/test/java/com/prgrms/album_market/order/OrderDataFactory.java
@@ -1,25 +1,25 @@
 package com.prgrms.album_market.order;
 
-import com.prgrms.album_market.album.AlbumDataFactory;
-import com.prgrms.album_market.member.MemberDataFactory;
+import com.prgrms.album_market.album.entity.Album;
+import com.prgrms.album_market.member.entity.Member;
 import com.prgrms.album_market.order.dto.OrderRequest;
 import com.prgrms.album_market.order.entity.Order;
 
 public class OrderDataFactory {
 
-    public static Order order(){
+    public static Order order(Member member, Album album, int quantity){
         return Order.builder()
-                .member(MemberDataFactory.getMember())
-                .album(AlbumDataFactory.getAlbum())
-                .quantity(1)
+                .member(member)
+                .album(album)
+                .quantity(quantity)
                 .build();
     }
 
-    public static OrderRequest.CreateOrderReq createOrderReq(){
+    public static OrderRequest.CreateOrderReq createOrderReq(int quantity){
         return OrderRequest.CreateOrderReq.builder()
                 .memberId(1L)
                 .albumId(1L)
-                .quantity(1)
+                .quantity(quantity)
                 .build();
     }
 
--- album_market/src/test/java/com/prgrms/album_market/order/service/OrderServiceTest.java
+++ album_market/src/test/java/com/prgrms/album_market/order/service/OrderServiceTest.java
@@ -1,9 +1,9 @@
 package com.prgrms.album_market.order.service;
 
-import com.prgrms.album_market.album.AlbumDataFactory;
 import com.prgrms.album_market.album.entity.Album;
 import com.prgrms.album_market.album.service.AlbumService;
-import com.prgrms.album_market.member.MemberDataFactory;
+import com.prgrms.album_market.common.exception.CustomException;
+import com.prgrms.album_market.common.exception.ErrorCode;
 import com.prgrms.album_market.member.entity.Member;
 import com.prgrms.album_market.member.service.MemberService;
 import com.prgrms.album_market.order.entity.Order;
@@ -13,13 +13,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import static com.prgrms.album_market.album.AlbumDataFactory.album;
+import static com.prgrms.album_market.member.MemberDataFactory.member;
 import static com.prgrms.album_market.order.OrderDataFactory.createOrderReq;
-import static com.prgrms.album_market.order.OrderDataFactory.order;
 import static com.prgrms.album_market.order.dto.OrderResponse.CreateOrderRes;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
@@ -35,16 +38,38 @@ class OrderServiceTest {
 
     @Test
     void createOrderSuccess(){
-        Member mockMember = MemberDataFactory.getMember();
-        Album mockAlbum = AlbumDataFactory.getAlbum();
-        Order mockOrder = order();
+        Member member = member();
+        ReflectionTestUtils.setField(member, "balance", 500000);
 
-        when(memberService.getMemberEntity(1L)).thenReturn(mockMember);
-        when(albumService.getAlbumEntity(1L)).thenReturn(mockAlbum);
-        when(orderRepository.save(any(Order.class))).thenReturn(mockOrder);
+        Album album = album(21000);
+        ReflectionTestUtils.setField(album, "stock",4);
 
-        CreateOrderRes response = orderService.createOrder(createOrderReq());
+        given(memberService.getMemberEntity(1L)).willReturn(member);
+        given(albumService.getAlbumEntity(1L)).willReturn(album);
 
-        assertEquals(mockOrder.getId(), response.getCreatedOrderId());
+        CreateOrderRes response = orderService.createOrder(createOrderReq(3));//order생성
+
+        assertNotNull(response);
+        verify(orderRepository).save(any(Order.class));
+    }
+
+    @Test
+    void createOrderFail(){
+        //given
+        Member member = member();
+        ReflectionTestUtils.setField(member, "balance", 500000);
+
+        Album album = album(21000);
+        ReflectionTestUtils.setField(album, "stock",1);
+
+        given(memberService.getMemberEntity(1L)).willReturn(member);
+        given(albumService.getAlbumEntity(1L)).willReturn(album);
+
+        //when
+        CustomException exception = assertThrows(CustomException.class, ()
+                -> orderService.createOrder(createOrderReq(5)));
+
+        //then
+        assertEquals(ErrorCode.OUT_OF_STOCK,exception.getErrorCode());
     }
 }
